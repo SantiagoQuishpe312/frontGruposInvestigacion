dcdf668d0f6ec0e5868983ab2dbc03c3b501ca38
estevennac <93159877+estevennac@users.noreply.github.com>
2025-04-21 22:49:20 -0500

    Vista Formulario Bienes
    Vista Formulario Bienes, Vista Memorando UTIC

--- src/@vex/components/forms/assetsReport/assetsReport.component.ts
+++ src/@vex/components/forms/assetsReport/assetsReport.component.ts
@@ -1,6 +1,8 @@
 import { Component,Input,OnInit } from "@angular/core";
 import { AssetsReportService } from "src/app/core/http/assets-report/assets-report.service";
-import { AssetsReportComplete } from "src/app/types/assetsReport.types";
+import { InvGroupService } from "src/app/core/http/inv-group/inv-group.service";
+import { AssetesDetails } from "src/app/types/assetesDetails.types";
+import { AssetsReport, AssetsReportComplete } from "src/app/types/assetsReport.types";
 
 @Component({
     selector: 'vex-reporte-bienes',
@@ -9,18 +11,37 @@ import { AssetsReportComplete } from "src/app/types/assetsReport.types";
   })
   export class ReporteBienesComponent implements OnInit{
     @Input() idGrupo!: number;
-    informeBienes:AssetsReportComplete;
+    assetsReports: AssetsReport[] = [];
+    AssetDetails: AssetesDetails[] = [];
+    groupName: string = '';
+
     constructor(
         private assetsReportService:AssetsReportService,
+        private groupService: InvGroupService
     ){}
     ngOnInit(): void {
         this.loadData();
+        this.loadGroupInfo();
         console.log(this.idGrupo);
     }
     loadData(){
         this.assetsReportService.getAllByGroup(this.idGrupo).subscribe(data=>{
-            this.informeBienes=data;
+            this.assetsReports = [data.reporteBienes];
+            this.AssetDetails = data.detalleBienes;
             console.log(data);
         })
+    }
+
+    loadGroupInfo() {
+        this.groupService.getById(this.idGrupo).subscribe(group => {
+          this.groupName = group.nombreGrupoInv;
+          console.log('Nombre del grupo:', this.groupName);
+        }, error => {
+          console.error('Error al cargar la información del grupo:', error);
+        });
+      }
+
+    onChange(event: any) {
+        const selectedReportId = event.target.value;
     }
   }
