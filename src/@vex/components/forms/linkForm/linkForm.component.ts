8330925419303a66ea270c7e96c1eeabcb458408
estevennac <93159877+estevennac@users.noreply.github.com>
2025-04-23 11:01:18 -0500

    Vista Vinculacion
    Vista Vinculacion

--- src/@vex/components/forms/linkForm/linkForm.component.ts
+++ src/@vex/components/forms/linkForm/linkForm.component.ts
@@ -1,7 +1,10 @@
 import { Component,Input,OnInit } from "@angular/core";
 import { InvGroupService } from "src/app/core/http/inv-group/inv-group.service";
 import { LinkService } from "src/app/core/http/link/link.service";
+import { UsuarioService } from "src/app/core/http/usuario/usuario.service";
+import { Area } from "src/app/types/area.types";
 import { LinksComplete } from "src/app/types/link.types";
+import { Usuario } from "src/app/types/usuario.types";
 @Component({
     selector: 'vex-vinculacion-form-view',
     templateUrl: './linkForm.component.html',
@@ -11,22 +14,50 @@ import { LinksComplete } from "src/app/types/link.types";
     @Input() idGrupo!: number;
     @Input() tipo!: string;
     @Input() estado!: string;
-    vinculacionForm: LinksComplete;
+    vinculacionForm: LinksComplete | null = null;
+    coordinador: Usuario | null = null;
+    Area: Area[] = [];
+    Usuarios: Usuario[] = [];
+
     groupName: string = '';
 
     constructor(
         private linkService:LinkService,
-        private groupService: InvGroupService
+        private groupService: InvGroupService,
+        private usuarioService: UsuarioService
+
     ){}
     ngOnInit(): void {
         this.loadData();
     }
-    loadData(){
-        this.linkService.getAllByGroup(this.idGrupo,this.estado,this.tipo).subscribe(data=>{
-            //this.vinculacionForm = data;
-            console.log(data);
-        })
+    loadData() {
+        this.linkService.getAllByGroup(this.idGrupo, this.estado, this.tipo).subscribe(data => {
+          if (Array.isArray(data) && data.length > 0) {
+            this.vinculacionForm = data[0];
+          } else {
+            this.vinculacionForm = data as unknown as LinksComplete;
+          }
+          
+          console.log('Loaded vinculacionForm:', this.vinculacionForm);
+          
+          if (this.vinculacionForm && this.vinculacionForm.grupoInvestigacion) {
+            const grupoInfo = this.vinculacionForm.grupoInvestigacion;
+            
+            if (grupoInfo.invGroup) {
+              this.groupName = grupoInfo.invGroup.nombreGrupoInv;
+            }
+            
+            if (grupoInfo.area && Array.isArray(grupoInfo.area)) {
+              this.Area = grupoInfo.area;
+            }
+            
+            if (grupoInfo.coordinador) {
+              this.coordinador = grupoInfo.coordinador;
+              this.Usuarios = [grupoInfo.coordinador];
+            }
+          }
+        }, error => {
+          console.error('Error loading data:', error);
+        });
+      }
     }
-
-
-  }
